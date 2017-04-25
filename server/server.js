549545c7172966c49fbf915eb5a805e9fc184bfc
Pierry Etienne <pierry.etienne@gmail.com>
2017-04-24 20:50:09 -0400

    Created serever data and created Angular module

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ app.get('/', function(request, response) {
 	response.sendFile(__dirname + '/index.html');
 });
 
+// app.use(express.static('/'));
+
+app.get('/nbateams', function(request, response) {
+	var nbaTeams = [{teamName: 'Hawks',
+		city: 'Atlanta'}, {teamName: 'Celtics', city: 'Boston'}, {teamName: 'Nets', city: 'Brooklyn'}, {teamName: 'Hornets', city: 'Charlotte'}, {teamName: 'Bulls', city: 'Chicago'}, {teamName: 'Cavaliers', city: 'Cleveland'}, {teamName: 'Mavericks', city: 'Dallas'}, {teamName: 'Nuggets', city: 'Denver'}, {teamName: 'Pistons', city: 'Detroit'}, {teamName: 'Warriors', city: 'Golden State'}, {teamName: 'Rockets', city: 'Houston'}, {teamName: 'Pacers', city: 'Indiana'}, {teamName: 'Clippers', city: 'Los Angeles'}, {teamName: 'Lakers', city: 'Los Angeles'}, {teamName: 'Grizzlies', city: 'Memphis'}, {teamName: 'Heat', city: 'Miami'}, {teamName: 'Bucks', city: 'Milwaukee'}, {teamName: 'Timberwolves', city: 'Minnesota'}, {teamName: 'Pelicans', city: 'New Orleans'}, {teamName: 'Knicks', city: 'New York'}, {teamName: 'Thunder', city: 'Oklahoma City'}, {teamName: 'Magic', city: 'Orlando'}, {teamName: 'Philadelphia', city: '76ers'}, {teamName: 'Suns', city: 'Phoenix'}, {teamName: 'Trail Blazers', city: 'Portland'}, {teamName: 'Kings', city: 'Sacramento'}, {teamName: 'Spurs', city: 'San Antonio'}, {teamName: 'Raptors', city: 'Toronto'}, {teamName: 'Jazz', city: 'Utah'}];
+
+	response.end(JSON.stringify({data: nbaTeams}));
+});
+
 app.listen(3000, function(){
 	console.log('Listening on port 3000');
 });
